f07057b14531e232a4856f6625a1a9d610a79e47
Chris Jones <cjfj@google.com>
2026-04-09 06:56:19 -0700

    [pallas:mgpu] Add support for specifying a cache eviction policy in `copy_gmem_to_smem`.
    PiperOrigin-RevId: 897089532

--- jax/experimental/pallas/mosaic_gpu.py
+++ jax/experimental/pallas/mosaic_gpu.py
@@ -98,6 +98,7 @@ from jax._src.state.types import Transform as Transform
 from jax.experimental.mosaic.gpu.core import LoweringSemantics as LoweringSemantics
 from jax.experimental.mosaic.gpu.fragmented_array import Replicated as Replicated
 from jax.experimental.mosaic.gpu.fragmented_array import Tiling as Tiling
+from jax.experimental.mosaic.gpu.launch_context import CachePolicy as CachePolicy
 from jax.experimental.mosaic.gpu.launch_context import CopyPartition as CopyPartition
 
 
